6e298301b6c6b7800bb4496c1ccc0e84783f5ec7
Стаснислав <stasyk.stas@mail.ru>
2023-05-04 02:44:55 +0900

    add main import_2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from funcs import addition_of_numbers
+from funcs import addition_of_numbers, addition_of_numbers_1
 
 numbers = addition_of_numbers(1, 6)
 print(numbers)
+
+numbers_1 = addition_of_numbers_1(6, 1)
+print(numbers_1)
