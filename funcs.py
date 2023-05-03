cb9e78a4bce751c13272392b07dee77ac5902e16
Стаснислав <stasyk.stas@mail.ru>
2023-05-04 02:42:55 +0900

    add main import

--- funcs.py
+++ funcs.py
@@ -2,6 +2,6 @@ def addition_of_numbers(a, b):
     num = a + b
     return num
 
-def addition_of_numbers(a, b):
+def addition_of_numbers_1(a, b):
     num_1 = a - b
     return num_1
---
+++ main.py
@@ -0,0 +1,4 @@
+from funcs import addition_of_numbers
+
+numbers = addition_of_numbers(1, 6)
+print(numbers)
